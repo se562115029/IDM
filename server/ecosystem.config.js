66e3a5f168c26a00c7bcd13a3f108e47dea48f78
Panadta Tajumpoo <panadtatajumpoo@Panadtas-Laptop.modem>
2024-10-13 21:45:31 +1100

    update config file

---
+++ server/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    app: [
+        {
+            name: "inventory-management",
+            script: "npm",
+            args: "run dev",
+            env: {
+                NODE_ENV: "development",
+                ENV_VAR1: "environment-variable",
+            }
+
+        }
+    ]
+d
