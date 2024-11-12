fddd4806e3b8f9afbd4c8b21a886915f7a5b26cc
Panadta Tajumpoo <panadtatajumpoo@Panadtas-Laptop.modem>
2024-11-12 18:15:29 +1100

    Change favicon

--- client/src/app/_components/Navbar/index.tsx
+++ client/src/app/_components/Navbar/index.tsx
@@ -30,17 +30,17 @@ const Navbar = () => {
           <Menu className="w-4 h-4"></Menu>
         </button>
 
-        <div className="relative">
-          {/* <input
+        {/* <div className="relative">
+          <input
             type="search"
             placeholder="Start type to search groups $ products"
             className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 boarder-gray-300 bg-white rounded-lg
             focus:outline-none focus:border-blue-500"
-          /> */}
-          {/* <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+          />
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
             <Bell className="text-gray-500" size={20}></Bell>
-          </div> */}
-        </div>
+          </div>
+        </div> */}
       </div>
       {/** RIGHT SIDE */}
       <div className="flex justify-between items-center gap-5">
